c8e296fc350603600e9377691cf14fe4e8583d85
pipoconsta <pipoconsta1@gmail.com>
2023-09-14 20:55:09 -0300

    BACKEND M3

---
+++ back/src/controllers/getCharById.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const URL = "https://rickandmortyapi.com/api/character"
+
+const getCharById =  async (req, res) =>{
+   try {
+  const {id} =req.params;
+   const {data} = await axios.get(`${URL}/${id}`)
+
+    let character = {
+       id:data.id,
+       name:data.name,
+       gender:data.gender,
+       origin:data.origin,
+       image:data.image,
+       status:data.status,
+       species:data.species
+    }
+   return character.name 
+   ? res.json(character).status(200) 
+   : res.status(404).send("Not found")
+
+} catch (error) {
+ res.status(500).send(error.message)
+}
+};
+
+module.exports = getCharById
